0d3abf58bf9782d9d4e0648853f055610157217f
Aditya Gupta <aditya27gupta@gmail.com>
2025-10-22 13:42:27 +0530

    Updated structs

---
+++ Structs/structs_test.go
@@ -0,0 +1,25 @@
+package structs
+
+import "testing"
+
+type Shape interface {
+	Area() float64
+}
+
+func TestArea(t *testing.T) {
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{shape: Rectangle{12, 6}, want: 72.0},
+		{shape: Circle{10}, want: 314.1592653589793},
+		{shape: Triangle{12, 6}, want: 36.0},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("%#v got %g, want %g", tt.shape, got, tt.want)
+		}
+	}
+}
